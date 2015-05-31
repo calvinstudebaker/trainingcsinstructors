7a73064c52a0a0212ad06477fd49eeaa4bca2ada
Calvin Studebaker <calstud@stanford.edu>
2015-05-31 16:01:32 -0700

    more site content

--- js/general.js
+++ js/general.js
@@ -1,22 +1,5 @@
-function switchToPage(page){
-	$(".content").hide("fast");
-	setTimeout(function() {
-		$(location).attr('href', page);
-	}, 300);
-};
 
-$(document).ready(function(){
-	$("ul.menuBar li").onclick(function(){
-
-	});
-});
-
-$('#myTab a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
-})
-
-$('#myTab a[href="#profile"]').tab('show') // Select tab by name
-$('#myTab a:first').tab('show') // Select first tab
-$('#myTab a:last').tab('show') // Select last tab
-$('#myTab li:eq(2) a').tab('show') // Select third tab (0-indexed)
+function setProgressBar(value){
+	var widthString = "width: " + value + "%;";
+	$("#progressBar").html(value + "%").attr("style", widthString).attr("aria-valuenow", String(value));
+}
