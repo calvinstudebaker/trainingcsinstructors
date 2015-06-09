dc04014d2a6fb36db0083a335d681a367d0d7f0d
Mikala Streeter <mpstreeter@gmail.com>
2015-06-09 10:26:01 -0700

    Add navigation between tabs at page bottom

--- js/general.js
+++ js/general.js
@@ -3,3 +3,11 @@ function setProgressBar(value){
 	var widthString = "width: " + value + "%;";
 	$("#progressBar").html(value + "%").attr("style", widthString).attr("aria-valuenow", String(value));
 }
+
+$(function () {
+    $('a').click(function (e) {
+        e.preventDefault();
+        $('a[href="' + $(this).attr('href') + '"]').tab('show');
+        window.scrollTo(0, 0);
+    })
+});
